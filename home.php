0afbcb303905f60cddd7ec5217ebbc9b504a01ec
rahulsai507 <37442722+rahulsai507@users.noreply.github.com>
2018-03-16 17:19:38 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="styles.css">
+        <style>
+            body{
+                background:url("home1.jpg");
+               background-size:100%;
+                background-repeat:no-repeat;
+            }
+        </style>
+    </head>
+    <body>
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="home.php">Home</a></li>
+                    <li><a href="loginpage.php">LogIn</a></li>
+                    <li><a href="signuppage.php">SignUp</a></li>
+                </ul>
+            </nav>
+        </header>
+    </body>
+</html>
